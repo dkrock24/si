5343ae476113d7feda4159d7f6f860d47e71aa10
esteba24 <rgutierreztejada@gmail.com>
2019-12-30 18:23:50 -0600

    Fix Datetime

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -143,7 +143,7 @@
       for(var i in months){
         if(i ==  momentNow.format('MMMM') ){
           console.log(months[i].Translate);
-          $('#time-part').html(months[i].Translate);
+          $('#time-part').html(months[i].Translate +" "+ momentNow.format('DD'));
         }
       }
 
