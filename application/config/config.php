2844d4a11aedc98fec6150cc4281d81a179f1840
Rafael.Gutierrez <rgutierreztejada@gmail.com>
2021-05-03 21:58:08 -0600

    remover ip del config

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://192.168.1.15:8081/index.php/';
+$config['base_url'] = 'http://localhost:8081/index.php/';
 
 /*
 |--------------------------------------------------------------------------
