6aba5023dc8265b630af6399ecc1584ec2a96398
Rafael.Gutierrez <rgutierreztejada@gmail.com>
2021-05-07 23:23:51 -0600

    Fix landing

--- application/models/reservas/Configuracion_model.php
+++ application/models/reservas/Configuracion_model.php
@@ -2,12 +2,27 @@
 class Configuracion_model extends CI_Model {
 
     const configuracion = "reserva_configuracion";
+    const sucursal = 'pos_sucursal';
 
     function get_configuracion($param){
         $this->db->select('valor_configuracion');
         $this->db->from( self::configuracion );
         $this->db->where('nombre_configuracion', $param);
         $this->db->where('Sucursal', $this->session->usuario[0]->Sucursal);
+        $query = $this->db->get(); 
+        
+        if($query->num_rows() > 0 )
+        {
+            return $query->result();
+        }
+    }
+
+    function get_configuracion_externa($param,$empresa){
+        $this->db->select('valor_configuracion');
+        $this->db->from( self::configuracion.' as config');
+        $this->db->join(self::sucursal.' as sucursal',' on sucursal.id_sucursal = config.Sucursal');
+        $this->db->where('nombre_configuracion', $param);
+        $this->db->where('Empresa_Suc', $empresa);
         $query = $this->db->get(); 
         
         if($query->num_rows() > 0 )
