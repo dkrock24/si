c2d8fdb4bb81ef914d9447e6a849365868acb23a
Rafael.Gutierrez <rgutierreztejada@gmail.com>
2021-05-20 10:47:12 -0600

    fix nueva habitacion

--- application/views/reservas/habitacion/nuevo.php
+++ application/views/reservas/habitacion/nuevo.php
@@ -7,6 +7,10 @@
         width: 25px;
         background-color: #eee;
     }
+
+    .table thead tr th{
+        color: #fff;
+    }
 </style>
 <section>
     <!-- Page content-->
@@ -14,13 +18,13 @@
 
         <h3 style="height: 50px; font-size: 13px;">
             <a name="reservas/habitacion/index" style="top: -12px;position: relative; text-decoration: none" class="holdOn_plugin">
-                <button type="button" class="mb-sm btn btn-success"> Habitacion</button> </a>
+                <button type="button" class="mb-sm btn btn-success"> Habitación</button> </a>
             <button type="button" style="top: -12px; position: relative;" class="mb-sm btn btn-info"> Nuevo</button>
         </h3>
         <div class="row">
             <div class="col-lg-12">
                 <div id="panelDemo10" class="panel menu_title_bar">
-                    <div class="panel-heading menuTop">Nuevo Habitacion </div>
+                    <div class="panel-heading menuTop">Nuevo Habitación </div>
                     <div class="panel-body menuContent">
                         <form class="form-horizontal" id='habitacion' method="post">
                             <div class="row">
@@ -29,7 +33,7 @@
                                         <div class="form-group">
                                             <label for="inputEmail3" class="col-sm-2 control-label no-padding-right">Nombre</label>
                                             <div class="col-sm-10">
-                                                <input type="text" class="form-control" id="nombre_habitacion" name="nombre_habitacion" placeholder="Nombre Habitacion" value="">
+                                                <input type="text" class="form-control" id="nombre_habitacion" name="nombre_habitacion" placeholder="Nombre Habitación" value="">
 
                                             </div>
                                         </div>
@@ -37,7 +41,7 @@
                                         <div class="form-group">
                                             <label for="inputEmail3" class="col-sm-2 control-label no-padding-right">Descripcion</label>
                                             <div class="col-sm-10">
-                                                <input type="text" class="form-control" id="descripcion_habitacion" name="descripcion_habitacion" placeholder="Descripcion Habitacion" value="">
+                                                <input type="text" class="form-control" id="descripcion_habitacion" name="descripcion_habitacion" placeholder="Descripcion Habitación" value="">
 
                                             </div>
                                         </div>
@@ -75,17 +79,17 @@
                                         </div>
 
                                         <div class="form-group">
-                                            <label for="inputEmail3" class="col-sm-2 control-label no-padding-right">Tamaño Habitacion</label>
+                                            <label for="inputEmail3" class="col-sm-2 control-label no-padding-right">Tamaño Habitación</label>
                                             <div class="col-sm-10">
-                                                <input type="text" class="form-control" id="tamano_habitacion" name="tamano_habitacion" placeholder="Tamaño Habitacion" value="">
+                                                <input type="text" class="form-control" id="tamano_habitacion" name="tamano_habitacion" placeholder="Tamaño Habitación" value="">
 
                                             </div>
                                         </div>
 
                                         <div class="form-group">
-                                            <label for="inputEmail3" class="col-sm-2 control-label no-padding-right">Capacidad Habitacion</label>
+                                            <label for="inputEmail3" class="col-sm-2 control-label no-padding-right">Capacidad Habitación</label>
                                             <div class="col-sm-10">
-                                                <input type="text" class="form-control" id="capacidad_habitacion" name="capacidad_habitacion" placeholder="Capacidad Habitacion" value="">
+                                                <input type="text" class="form-control" id="capacidad_habitacion" name="capacidad_habitacion" placeholder="Capacidad Habitación" value="">
 
                                             </div>
                                         </div>
@@ -110,26 +114,30 @@
                                 </div>
                                 <div class="col-lg-6">
                                     <table class="table">
-                                    <tr>
-                                        <td>#</td>
-                                        <td>Articulo Habitacion</td>
-                                        <td>Modelo</td>
-                                        <td>Cantidad</td>
-                                    </tr>
-                                    <?php
-                                    foreach ($articulos as $key => $articulo) {
-                                        ?>
+                                    <thead style="background:#4974a7;">
                                         <tr>
-                                            <td class="box-padding">
-                                                <input type="checkbox" class="input-check" name="articulo-<?php echo $key; ?>" value="<?php echo $articulo->id_reserva_articulo; ?>">
-                                            </td>
-                                            <td><?php echo $articulo->nombre_articulo; ?></td>
-                                            <td><?php echo $articulo->modelo_articulo; ?></td>
-                                            <td>
-                                                <input type="number" name="cantidad-<?php echo $key; ?>" value="1">
-                                            </td>
+                                            <th>#</th>
+                                            <th>Articulo Habitación</th>
+                                            <th>Modelo</th>
+                                            <th>Cantidad</th>
                                         </tr>
-                                        <?php
+                                    </thead>
+                                    <?php
+                                    if($articulos) {
+                                        foreach ($articulos as $key => $articulo) {
+                                            ?>
+                                            <tr>
+                                                <td class="box-padding">
+                                                    <input type="checkbox" class="input-check" name="articulo-<?php echo $key; ?>" value="<?php echo $articulo->id_reserva_articulo; ?>">
+                                                </td>
+                                                <td><?php echo $articulo->nombre_articulo; ?></td>
+                                                <td><?php echo $articulo->modelo_articulo; ?></td>
+                                                <td>
+                                                    <input type="number" name="cantidad-<?php echo $key; ?>" value="1">
+                                                </td>
+                                            </tr>
+                                            <?php
+                                        }
                                     }
                                     ?>
                                     </table>
